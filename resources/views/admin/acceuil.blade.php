86dade8efde43e2a4de20f3d11d1c11e3caa1817
Ny-Aritoky Michael RJR <89502733+Ny-AritokyMichael@users.noreply.github.com>
2023-07-21 11:02:49 +0300

    Remplacer

--- resources/views/admin/acceuil.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('admin.header.app')
-
-
-@section('content')
-    {{-- <h1>Liste des voitures disponibles</h1> --}}
-@endsection
--- resources/views/erreur/erreurKilo.blade.php
+++
@@ -1,2 +0,0 @@
-<h1>Erreur de kilometrage</h1>
-<a href="#">Reessayer</a>
